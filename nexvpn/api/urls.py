1934a48359407dc326b384c73c553ced4f3f8b5f
arsenyvolodko <arvolodko@gmail.com>
2025-04-04 17:50:22 +0500

    add extra tmp endpoint for yookassa

--- nexvpn/api/urls.py
+++ nexvpn/api/urls.py
@@ -3,8 +3,10 @@ from django.urls.conf import include
 
 from .admin.urls import urlpatterns as admin_urls
 from .notifications.urls import urlpatterns as yookassa_urls
+from .notifications.views import handle_notification
 
 urlpatterns = [
     path("admin/", include(admin_urls)),
+    path("/api/v1/payment_succeeded/", handle_notification),
     path("yookassa/", include(yookassa_urls)),
 ]
