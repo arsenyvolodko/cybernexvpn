606c797e3c58afaae1bd1f7e2ae0bc2f0f87e832
arsenyvolodko <arvolodko@gmail.com>
2025-09-26 12:54:36 +0300

    feat yookassa middleware

---
+++ cybernexvpn/middleware.py
@@ -0,0 +1,28 @@
+import logging
+import ipaddress
+
+
+logger = logging.getLogger(__name__)
+
+
+class YookassaCallbackMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    @staticmethod
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        logger.info(f"HTTP_X_FORWARDED_FOR: {x_forwarded_for}")
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(",")[0]
+            return ipaddress.ip_address(ip)
+        return request.META.get("REMOTE_ADDR")
+
+    def __call__(self, request):
+        if request.path.endswith("yookassa/callback"):
+            client_ip = self.get_client_ip(request)
+            logger.info(f"client_ip: {client_ip}")
+
+        response = self.get_response(request)
+        return response
