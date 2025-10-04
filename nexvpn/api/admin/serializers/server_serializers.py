44def1d37145e60f3bdbe8fd32214467a25fe6ce
arsenyvolodko <arvolodko@gmail.com>
2025-10-04 15:53:58 +0300

    add has_available_ips to server serializer

--- nexvpn/api/admin/serializers/server_serializers.py
+++ nexvpn/api/admin/serializers/server_serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
-from nexvpn.models import Server
+from nexvpn.models import Server, Endpoint
 
 
 class ServerSerializer(serializers.ModelSerializer):
 
+    has_available_ips = serializers.SerializerMethodField()
+
+    @staticmethod
+    def get_has_available_ips(obj: Server):
+        return Endpoint.objects.filter(
+            server_id=obj.id, client__isnull=True
+        ).exists()
+
     class Meta:
         model = Server
-        fields = ["id", "name", "price", "tag", ]
+        fields = ["id", "name", "price", "tag", "has_available_ips"]
